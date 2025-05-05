facb85aa06b0b74e41fc94902e58a4944e39595c
deeraj1899 <151001717+deeraj1899@users.noreply.github.com>
2025-05-05 21:49:01 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,7 +5,7 @@ import { PersistGate } from 'redux-persist/integration/react';
 import App from './App.jsx';
 import { persistStore } from 'redux-persist';
 import './index.css';
-import store from './Redux/Store.js';
+import store from './redux/store.js';
 const persistor = persistStore(store); 
 
 createRoot(document.getElementById('root')).render(
